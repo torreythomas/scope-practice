7932f2e500f8479eaad42167995f7b3fa8dde61c
Torreythms <torreythms@gmail.com>
2020-05-04 09:47:45 -0400

    prompt 1

--- scope-practice/exercise1.js
+++ scope-practice/exercise1.js
@@ -1,6 +1,11 @@
 
-// For all of these, what is the value of a when the function gets called with the alert()
+// For all of these, what is the value of "a" when the function gets called with the alert()
 // #1
+
+
+console.log("living")
+
+
 function q1() {
     var a = 5;
     if(a > 1) {
@@ -9,38 +14,41 @@ function q1() {
     alert(a);
 }
 
-//#2
-var a = 0;
-function q2() {
-    a = 5;
-}
-
-function q22() {
-    alert(a);
-}
+/* value = 3 */
 
 
-//#3
-function q3() {
-    window.a = "hello";
-}
+// //#2
+// var a = 0;
+// function q2() {
+//     a = 5;
+// }
+
+// function q22() {
+//     alert(a);
+// }
 
 
-function q32() {
-    alert(a);
-}
+// //#3
+// function q3() {
+//     window.a = "hello";
+// }
 
-//#4
-var a = 1;
-function q4() {
-    var a = "test";
-    alert(a);
-}
 
-//#5
-var a = 2;
-if (true) {
-    var a = 5;
-    alert(a);
-}
-alert(a);
+// function q32() {
+//     alert(a);
+// }
+
+// //#4
+// var a = 1;
+// function q4() {
+//     var a = "test";
+//     alert(a);
+// }
+
+// //#5
+// var a = 2;
+// if (true) {
+//     var a = 5;
+//     alert(a);
+// }
+// alert(a);
